d13dd35afb819089257dc086798968992ecc296c
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-28 12:20:30 -0500

    I added thw needed includes to the top of the program, I also attempted to add some stuff for handing The command line arguments, But I do not think it is correct yet.  I added prinf() statements to print out the resultsx!

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -1,3 +1,7 @@
+#include<ftw.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 static int num_dirs, num_regular;
 static int callback(const char *fpath, const struct stat*sb, int typeflag){
   if(typeflag == FTW_D){
@@ -10,12 +14,19 @@ static int callback(const char *fpath, const struct stat*sb, int typeflag){
   return 0;
 }
 
-#define MAX_TFW_DEPTH 16
+#define MAX_FTW_DEPTH 16
 
 int main(int argc, char** argv){
   //check arguements and set things up 
-
+  if(argc != 2){
+    printf("need two arguements");
+    exit(0);
+  }
+  num_dirs = 0; 
+  num_regular =0; 
   ftw(argv[1], callback, MAX_FTW_DEPTH);
+  printf("number of dirs : %d" , num_dirs);
+  printf("number of files : %d", num_regular);
 
-  // Prit out the results  
+  return 0;
 }
