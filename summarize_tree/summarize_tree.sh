1ac1b9e8b0097111608f15f5e049c5c8cc2872d0
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-29 10:21:01 -0500

    I added paths to the bash commands not sure why I needed to do this, but it made it work. I also added the -l flag to the wc command so I was getting the correct output

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -3,10 +3,10 @@ HOME=$(pwd)
 PATH="$1"
 cd "$PATH" || exit
 
-FILE=$(find . -type f | wc) 
-DIR=$(find . -type d | wc:) 
+FILE_NUM=$( /usr/bin/find . -type f | /usr/bin/wc -l) 
+DIR_NUM=$( /usr/bin/find . -type d | /usr/bin/wc -l) 
 
-echo "There were" $DIR "directories."
-echo "There were" $FILE "regular files."
+echo "There were" "$DIR_NUM" "directories."
+echo "There were" "$FILE_NUM" "regular files."
 
 cd "$HOME" || exit 
